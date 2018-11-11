925d3b84dac94f77a9442a2ff0333f8d1f7fdd41
zxctja <zxctja@163.com>
2018-11-11 21:45:37 +0800

    env

---
+++ snap_env.sh
@@ -0,0 +1,5 @@
+export PSLVER=9
+export ACTION_ROOT=${SNAP_ROOT}/actions/hls_computing
+export PSLSE_ROOT=/home/zxc/pslse
+export TIMING_LABLIMIT="-200"
+#export PSL9_IP_CORE= <for CAPI2.0 cards: path to ibm.com_CAPI_PSL9_WRAP2.00.zip file>
